e1428057c658251facc314ebb25c81c333e12200
ArpanaMishra94 <arpanamishra394@gmail.com>
2023-04-29 12:58:27 +0530

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import logo from '../assets/neoFi logo-fotor-bg-remover-20230429124814.png';
+import { GiHamburgerMenu } from "react-icons/gi";
+import './Navbar.css'
+
+const Navbar = () => {
+    const [showMediaIcons, setShowMediaIcons] = useState(false);
+
+    return (
+        <div className='main-nav'>
+            <div className='logo'>
+                <img className='into-img' src={logo} alt='' />
+                <h2> NeoFi</h2>
+            </div>
+
+            <div className={
+                showMediaIcons ? "menu mobile-menu" : "menu"
+            }>
+                <ul>
+                    <li>Trade</li>
+                    <li>Earn</li>
+                    <li>Support</li>
+                    <li>About</li>
+                </ul>
+            </div>
+
+            <div className='wallet-btn'>
+                <button>Connect Wallet</button>
+                <div className="hamburger-menu" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                    <GiHamburgerMenu size={20} style={{ color: "#fff" }} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
